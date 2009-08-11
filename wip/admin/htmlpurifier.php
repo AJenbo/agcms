84bcbdf7d6913046f1207ecb84b3646848f664b2
Anders Jenbo <anders@jenbo.dk>
2009-08-11 10:56:38 +0000

    git-svn-id: http://jof.unfuddle.com/svn/jof_cms@18 31fb0081-5fcf-4b11-800b-6304723eda88

---
+++ wip/admin/htmlpurifier.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once '../inc/config.php';
+require_once '../inc/mysqli.php';
+$mysqli = new simple_mysqli($GLOBALS['_config']['mysql_server'], $GLOBALS['_config']['mysql_user'], $GLOBALS['_config']['mysql_password'], $GLOBALS['_config']['mysql_database']);
+
+if($_GET['id'])
+	$faktura = $mysqli->fetch_array("SELECT `text` FROM `sider` WHERE `id` = ".$_GET['id']);
+
+echo 'Før:
+';
+echo $faktura[0]['text'];
+
+
+
+require_once 'inc/htmlpurifier/HTMLPurifier.auto.php';
+
+
+$config = HTMLPurifier_Config::createDefault();
+//$config->set('Filter.YouTube', true);
+$config->set('HTML.SafeObject', true);
+$config->set('HTML.SafeEmbed', true);
+$config->set('HTML.Doctype', 'XHTML 1.0 Strict'); // replace with your doctype
+$purifier = new HTMLPurifier($config);
+
+$clean_html = $purifier->purify($faktura[0]['text']);
+echo '
+Efter:
+'.$clean_html;
+?>
---
+++ wip/admin/inc/htmlpurifier/HTMLPurifier/HTMLModule/SafeEmbed.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * A "safe" embed module. See SafeObject. This is a proprietary element.
+ */
+class HTMLPurifier_HTMLModule_SafeEmbed extends HTMLPurifier_HTMLModule
+{
+
+    public $name = 'SafeEmbed';
+
+    public function setup($config) {
+
+        $max = $config->get('HTML.MaxImgLength');
+        $embed = $this->addElement(
+            'embed', 'Inline', 'Empty', 'Common',
+            array(
+                'src*' => 'URI#embedded',
+                'type' => 'Enum#application/x-shockwave-flash',
+                'width' => 'Pixels#' . $max,
+                'height' => 'Pixels#' . $max,
+                'allowscriptaccess' => 'Enum#never',
+                'allownetworking' => 'Enum#internal',
+                'allowfullscreen' => 'Enum#true,false',
+                'quality' => 'Enum#low,high',
+                'bgcolor' => 'Text',
+                'pluginspage' => 'URL',
+                'wmode' => 'Enum#window',
+                'name' => 'ID',
+            )
+        );
+        $embed->attr_transform_post[] = new HTMLPurifier_AttrTransform_SafeEmbed();
+
+    }
+
+}
+
+// vim: et sw=4 sts=4
