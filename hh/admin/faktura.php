1cce063ca5402425a1250f6452b94869c32cda64
Anders Jenbo <anders@jenbo.dk>
2009-09-02 12:48:02 +0000

    git-svn-id: http://jof.unfuddle.com/svn/jof_cms@87 31fb0081-5fcf-4b11-800b-6304723eda88

--- hh/admin/faktura.php
+++ hh/admin/faktura.php
@@ -494,10 +494,10 @@ function save($id, $type, $updates) {
 		if(!validemail($faktura['email'])) {
 			return array('error' => 'Mail adressen er ikke gyldig!');
 		}
-		if(empty($faktura['department']) && count($GLOBALS['_config']['email']) > 1) {
+		if(!$faktura['department'] && count($GLOBALS['_config']['email']) > 1) {
 			return array('error' => 'Du har ikke valgt en afsender!');
-		} else {
-			$faktura['department'] = $GLOBALS['_config']['email'][0];
+		} elseif(!$faktura['department']) {
+				$faktura['department'] = $GLOBALS['_config']['email'][0];
 		}
 		if($faktura['amount'] < 1) {
 			return array('error' => 'Fakturaen skal være på mindst 1 krone!');
