e9fa9b867270115bc344efc7cf7edd84a98c3a41
Anders Jenbo <anders@jenbo.dk>
2009-11-12 17:24:41 +0000

    git-svn-id: http://jof.unfuddle.com/svn/jof_cms@126 31fb0081-5fcf-4b11-800b-6304723eda88

--- cms/admin/inc/logon.php
+++ cms/admin/inc/logon.php
@@ -13,34 +13,34 @@ session_start();
 
 if(empty($_SESSION['_user']) && !empty($_POST['username'])) {
 	$mysqli = new simple_mysqli($GLOBALS['_config']['mysql_server'], $GLOBALS['_config']['mysql_user'], $GLOBALS['_config']['mysql_password'], $GLOBALS['_config']['mysql_database']);
-	$GLOBALS['_user'] = $mysqli->fetch_array("SELECT `id`, `fullname`, `name`, `access`, `password` FROM `users` WHERE `access` != '0' AND `name` = '".addcslashes($_POST['username'], "'")."' LIMIT 1");
-	$GLOBALS['_user'] = @$GLOBALS['_user'][0];
-	$_SESSION['_user'] = $GLOBALS['_user'];
-} elseif(!empty($_SESSION['_user'])) {
-	$GLOBALS['_user'] = $_SESSION['_user'];
+
+	$_SESSION['_user'] = $mysqli->fetch_array("SELECT * FROM `users` WHERE `name` = '".addcslashes($_POST['username'], "'\\")."' LIMIT 1");
+	$_SESSION['_user'] = @$_SESSION['_user'][0];
+	if($_SESSION['_user']['access'] < 1 || mb_substr(@$_SESSION['_user']['password'], 0, 13) != mb_substr(crypt(@$_POST['password'], $_SESSION['_user']['password']), 0, 13))
+	 unset($_SESSION['_user']);
 }
 
-if(empty($GLOBALS['_user']['password']) || ($GLOBALS['_user']['password'] != crypt(mb_strtolower(@$_POST['password'], 'UTF-8'), $GLOBALS['_user']['password']))) {
-	unset($GLOBALS['_user']);
-	unset($_SESSION['_user']);
+if(empty($_SESSION['_user'])) {
+	sleep(1);
 	header('HTTP/1.0 401 Unauthorized');
-	die('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Login</title>
-</head>
-<body>
-<form action="" method="post" style="text-align:right; width:250px; margin:auto;">
-    Bruger:
-    <input name="username" />
-    <br />
-    Adgangskode:
-    <input type="password" name="password" />
-    <br />
-    <input type="submit" value="Log ind" />
-</form>
-</body>
-</html>');
+	?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Login</title>
+	</head>
+	<body>
+	<form action="" method="post" style="text-align:right; width:300px; margin:auto;">
+	 Bruger:
+	 <input name="username" />
+	 <br />
+	 Adgangskode:
+	 <input type="password" name="password" />
+	 <br />
+	 <input type="submit" value="Log ind" />
+	</form>
+	</body>
+	</html><?php
+	die();
 }
 ?>
