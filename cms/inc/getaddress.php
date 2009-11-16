bd7a677de29fa9bff4377d763a29cc1f769f885b
Anders Jenbo <anders@jenbo.dk>
2009-11-16 14:15:43 +0000

    Gather addresses from all 4 sites
    git-svn-id: http://jof.unfuddle.com/svn/jof_cms@128 31fb0081-5fcf-4b11-800b-6304723eda88

--- cms/inc/getaddress.php
+++ cms/inc/getaddress.php
@@ -1,74 +1,80 @@
 <?php
 function getAddress($id) {
+	
+	
+	$default['recName1'] = '';
+	$default['recAddress1'] = '';
+	$default['recZipCode'] = '';
+	$default['recCVR'] = '';
+	$default['recAttPerson'] = '';
+	$default['recAddress2'] = '';
+	$default['recPostBox'] = '';
+	$default['email'] = '';
+	
+	$dbs[0]['mysql_server'] = 'jagtogfiskerimagasinet.dk.mysql';
+	$dbs[0]['mysql_user'] = 'jagtogfiskerima';
+	$dbs[0]['mysql_password'] = 'GxYqj5EX';
+	$dbs[0]['mysql_database'] = 'jagtogfiskerima';
+	$dbs[1]['mysql_server'] = 'huntershouse.dk.mysql';
+	$dbs[1]['mysql_user'] = 'huntershouse_dk';
+	$dbs[1]['mysql_password'] = 'sabbBFab';
+	$dbs[1]['mysql_database'] = 'huntershouse_dk';
+	$dbs[2]['mysql_server'] = 'arms-gallery.dk.mysql';
+	$dbs[2]['mysql_user'] = 'arms_gallery_dk';
+	$dbs[2]['mysql_password'] = 'hSKe3eDZ';
+	$dbs[2]['mysql_database'] = 'arms_gallery_dk';
+	$dbs[3]['mysql_server'] = 'geoffanderson.com.mysql';
+	$dbs[3]['mysql_user'] = 'geoffanderson_c';
+	$dbs[3]['mysql_password'] = '2iEEXLMM';
+	$dbs[3]['mysql_database'] = 'geoffanderson_c';
+	
 	require_once 'mysqli.php';
-	require_once 'config.php';
-//	$mysqli_jof = new simple_mysqli('localhost', 'jagtogfiskerimag', '.460jagtogfiskeri', 'jagtogfiskerimaga_dk001');
-	$mysqli_jof = new simple_mysqli($GLOBALS['_config']['mysql_server'], $GLOBALS['_config']['mysql_user'], $GLOBALS['_config']['mysql_password'], $GLOBALS['_config']['mysql_database']);
-
-
-	//try packages
-	if($user = $mysqli_jof->fetch_array('SELECT recName1, recAddress1, recZipCode FROM `post` WHERE `recipientID` LIKE \''.$id.'\' ORDER BY id DESC LIMIT 1')) {
-		$return = $user[0];
-	//Try katalog orders
-	} elseif($user = $mysqli_jof->fetch_array('SELECT navn, email, adresse, post FROM `email` WHERE `tlf1` LIKE \''.$id.'\' OR `tlf2` LIKE \''.$id.'\' ORDER BY id DESC LIMIT 1')) {
-		$return['recName1'] = $user[0]['navn'];
-		$return['recAddress1'] = $user[0]['adresse'];
-		$return['recZipCode'] = $user[0]['post'];
-		$return['email'] = $user[0]['email'];
-	//Try fakturas
-	} elseif($user = $mysqli_jof->fetch_array('SELECT navn, email, att, adresse, postnr FROM `fakturas` WHERE `tlf1` LIKE \''.$id.'\' OR `tlf2` LIKE \''.$id.'\' ORDER BY id DESC LIMIT 1')) {
+	
+	foreach($dbs as $db) {
+		$mysqli_ext = new simple_mysqli($db['mysql_server'], $db['mysql_user'], $db['mysql_password'], $db['mysql_database']);
+		
+		//try packages
+		if($user = $mysqli_ext->fetch_array('SELECT recName1, recAddress1, recZipCode FROM `post` WHERE `recipientID` LIKE \''.$id.'\' ORDER BY id DESC LIMIT 1')) {
+			$return = array_merge($default, $user[0]);
+			if($return != $default)
+				return $return;
+		}
+		
+		//Try katalog orders
+		if($user = $mysqli_ext->fetch_array('SELECT navn, email, adresse, post FROM `email` WHERE `tlf1` LIKE \''.$id.'\' OR `tlf2` LIKE \''.$id.'\' ORDER BY id DESC LIMIT 1')) {
+			$return['recName1'] = $user[0]['navn'];
+			$return['recAddress1'] = $user[0]['adresse'];
+			$return['recZipCode'] = $user[0]['post'];
+			$return['email'] = $user[0]['email'];
+			$return = array_merge($default, $return);
+			
+			if($return != $default)
+				return $return;
+		}
+		
+		//Try fakturas
+		if($user = $mysqli_ext->fetch_array('SELECT navn, email, att, adresse, postnr, postbox FROM `fakturas` WHERE `tlf1` LIKE \''.$id.'\' OR `tlf2` LIKE \''.$id.'\' ORDER BY id DESC LIMIT 1')) {
 		$return['recName1'] = $user[0]['navn'];
 		$return['recAddress1'] = $user[0]['adresse'];
 		$return['recZipCode'] = $user[0]['postnr'];
 		$return['recAttPerson'] = $user[0]['att'];
 		$return['recPostBox'] = $user[0]['postbox'];
 		$return['email'] = $user[0]['email'];
-	//try HH
-	} else {
-		/*
-		$mysqli_hh = new simple_mysqli($GLOBALS['_config']['mysql_server'], 'huntershouse2_dk', '.460weatherby2', 'huntershouse2_dk001');
-		if($user = $mysqli_hh->fetch_array('SELECT navn, email, adresse, post FROM `email` WHERE `tlf1` LIKE \''.$id.'\' OR `tlf2` LIKE \''.$id.'\' ORDER BY id DESC LIMIT 1')) {
-			$return['recName1'] = $user[0]['navn'];
-			$return['recAddress1'] = $user[0]['adresse'];
-			$return['recZipCode'] = $user[0]['post'];
-			$return['email'] = $user[0]['email'];
-		//Try fakturas
-		} elseif($user = $mysqli_hh->fetch_array('SELECT navn, email, att, adresse, postnr FROM `fakturas` WHERE `tlf1` LIKE \''.$id.'\' OR `tlf2` LIKE \''.$id.'\' ORDER BY id DESC LIMIT 1')) {
-			$return['recName1'] = $user[0]['navn'];
-			$return['recAddress1'] = $user[0]['adresse'];
-			$return['recZipCode'] = $user[0]['postnr'];
-			$return['recAttPerson'] = $user[0]['att'];
-			$return['recPostBox'] = $user[0]['postbox'];
-			$return['email'] = $user[0]['email'];
-		//try krak
-		} else {
-		*/
-			require_once('../krak/krak.php');
-			$return = getAddressKrak($id);
-		//}
+		$return = array_merge($default, $return);
+			
+		if($return != $default)
+			return $return;
+		}
 	}
-	
-	if(empty($return['recName1']))
-		$return['recName1'] = '';
-	if(empty($return['recAddress1']))
-		$return['recAddress1'] = '';
-	if(empty($return['recZipCode']))
-		$return['recZipCode'] = '';
-	if(empty($return['recCVR']))
-		$return['recCVR'] = '';
-	if(empty($return['recAttPerson']))
-		$return['recAttPerson'] = '';
-	if(empty($return['recAddress2']))
-		$return['recAddress2'] = '';
-	if(empty($return['recPostBox']))
-		$return['recPostBox'] = '';
-	if(empty($return['email']))
-		$return['email'] = '';
 		
-	if($return['recName1'] || $return['recAddress1'] || $return['recZipCode'] || $return['recCVR'] || $return['recAttPerson'] || $return['recAddress2'] || $return['recPostBox'] || $return['email'])
-		return $return;
-	else
-		//None found return error
+	require_once('../krak/krak.php');
+	if($return = getAddressKrak($id)) {
+		$return = array_merge($default, $return);
+			
+		if($return != $default)
+			return $return;
+	} else {
 		return array('error' => 'Addressen kunde ikke findes.');
+	}
 }
 ?>
