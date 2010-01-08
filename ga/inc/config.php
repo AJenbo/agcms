36b9dfffe29867c03f0f0f391eb88aaf3249b98c
Anders Jenbo <anders@jenbo.dk>
2010-01-08 14:23:27 +0000

    Add emails to config.php
    git-svn-id: http://jof.unfuddle.com/svn/jof_cms@164 31fb0081-5fcf-4b11-800b-6304723eda88

--- ga/inc/config.php
+++ ga/inc/config.php
@@ -6,10 +6,19 @@ $GLOBALS['_config']['postcode'] = '2450';
 $GLOBALS['_config']['city'] = 'Copenhagen SV';
 $GLOBALS['_config']['phone'] = '+45 36 44 26 60';
 $GLOBALS['_config']['fax'] = '';
+
+$GLOBALS['_config']['email'][] = 'morten@geoffanderson.com';
+$GLOBALS['_config']['emailpasswords'][] = 'dozer01';
 $GLOBALS['_config']['email'][] = 'info@geoffanderson.com';
+$GLOBALS['_config']['emailpasswords'][] = 'dozer03';
+$GLOBALS['_config']['emailsent'] = 'INBOX.Sent';
+$GLOBALS['_config']['imap'] = 'imap.geoffanderson.com';
+$GLOBALS['_config']['imapport'] = '143';
+
 $GLOBALS['_config']['smtp'] = 'mailout.one.com';
 $GLOBALS['_config']['smtpport'] = 25;
 $GLOBALS['_config']['emailpassword'] = false;
+
 $GLOBALS['_config']['interests'][] = '';
 $GLOBALS['_config']['pbsid'] = '';
 $GLOBALS['_config']['pbspassword'] = '';
