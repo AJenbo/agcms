bd4d118e9764ce1559a155dfc5e6cf2e6c0b6977
Anders Jenbo <anders@jenbo.dk>
2010-06-14 15:42:27 +0000

    git-svn-id: http://jof.unfuddle.com/svn/jof_cms@266 31fb0081-5fcf-4b11-800b-6304723eda88

--- cms/admin/faktura-pdf.php
+++ cms/admin/faktura-pdf.php
@@ -1,4 +1,36 @@
 <?php
+date_default_timezone_set('Europe/Copenhagen');
+setlocale(LC_ALL, 'da_DK');
+bindtextdomain("agcms", $_SERVER['DOCUMENT_ROOT'].'/theme/locale');
+bind_textdomain_codeset("agcms", 'UTF-8');
+textdomain("agcms");
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/admin/inc/logon.php';
+
+if($_GET['id'] > 0) {
+	$id = $_GET['id'];
+} else  {
+	die(_('Wrong id.'));
+}
+
+require_once '../inc/config.php';
+require_once '../inc/mysqli.php';
+$mysqli = new simple_mysqli($GLOBALS['_config']['mysql_server'], $GLOBALS['_config']['mysql_user'], $GLOBALS['_config']['mysql_password'], $GLOBALS['_config']['mysql_database']);
+
+$faktura = $mysqli->fetch_one("SELECT *, UNIX_TIMESTAMP(`date`) AS `date`, UNIX_TIMESTAMP(`paydate`) AS `paydate` FROM `fakturas` WHERE `id` = ".$id." AND `status` != 'new'");
+if(!$faktura) {
+	die(_('Can\'t print.'));
+}
+
+$faktura['quantities'] = explode('<', $faktura['quantities']);
+$faktura['products'] = explode('<', $faktura['products']);
+$faktura['values'] = explode('<', $faktura['values']);
+	
+if(!$faktura['premoms']) {
+	foreach($faktura['values'] as $key => $value) {
+		$faktura['values'][$key] = $value*(1+$faktura['momssats']);
+	}
+}
 
 require_once('../inc/tcpdf/config/lang/dan.php');
 require_once('../inc/tcpdf/tcpdf.php');
@@ -8,8 +40,8 @@ $pdf = new TCPDF('P', 'mm', 'A4', true, 'UTF-8', false);
 
 // set document information
 $pdf->SetCreator(PDF_CREATOR);
-$pdf->SetAuthor('Jagt og Fiskerimagasinet');
-$pdf->SetTitle('Online faktura #1547');
+$pdf->SetAuthor($GLOBALS['_config']['site_name']);
+$pdf->SetTitle('Online faktura #'.$faktura['id']);
 
 // remove default header/footer
 $pdf->setPrintHeader(false);
@@ -35,58 +67,97 @@ $pdf->AddPage();
 
 //Site title
 $pdf->SetFont('times', 'B', 37.5);
-$pdf->Write(0, 'Jagt og Fiskerimagasinet');
+$pdf->Write(0, $GLOBALS['_config']['site_name']);
+
+//Contact info
+$pdf->SetY(12);
+$pdf->SetFont('times', '', 10);
+$pdf->Write(0, 
+	$GLOBALS['_config']['address']."\n".
+	$GLOBALS['_config']['postcode']." ".$GLOBALS['_config']['city']."\n".
+	"Fax: ".$GLOBALS['_config']['fax']."\n", '', 0, 'R');
+$pdf->SetFont('times', '', 11);
+$pdf->Write(0, _('Phone:')." ".$GLOBALS['_config']['phone']."\n", '', 0, 'R');
+$pdf->SetFont('times', '', 10);
+
+if(empty($faktura['department'])) {
+	$faktura['department'] = $GLOBALS['_config']['email'][0];
+}
+$domain = explode('/', $GLOBALS['_config']['base_url']);
+$domain = $domain[count($domain)-1];
+$pdf->Write(0, $faktura['department']."\n".$domain."\n\n", '', 0, 'R');
+$pdf->SetFont('times', '', 11);
+$pdf->Write(0, "Danske Bank (Giro)\nReg.: 9541 Kont.: 169 3336\n", '', 0, 'R');
+$pdf->SetFont('times', '', 10);
+$pdf->Write(0, "\nIBAN: DK693 000 000-1693336\nSWIFT BIC: DABADKKK\n\n", '', 0, 'R');
+$pdf->SetFont('times', 'B', 11);
+$pdf->Write(0, "CVR 1308 1387", '', 0, 'R');
 
 //Seperation lines
 $pdf->SetLineWidth(0.5);
 //Horizontal
 $pdf->Line(8, 27, 150, 27);
 //Vertical
-$pdf->Line(154, 12, 154, 80);
-
-//Contact info
-$pdf->SetY(12);
-$pdf->SetFont('times', '', 10);
-$pdf->Write(0, "Nørre Voldgade 8-10\n1358 København K\nFax: +45 33 14 04 07\n", '', 0, 'R');
-$pdf->SetFont('times', '', 11);
-$pdf->Write(0, "Telefon: 33 33 77 77\n", '', 0, 'R');
-$pdf->SetFont('times', '', 10);
-$pdf->Write(0, "mail@jagtogfiskerimagsinet.dk\n\n", '', 0, 'R');
-$pdf->SetFont('times', '', 11);
-$pdf->Write(0, "Danske Bank (Giro)\nReg.: 9541 Kont.: 169 3336\n", '', 0, 'R');
-$pdf->SetFont('times', '', 10);
-$pdf->Write(0, "\nIBAN:\nDK693 000 000- 1693336\nSWIFT BIC:\nDABADKKK\n\n", '', 0, 'R');
-$pdf->SetFont('times', 'B', 11);
-$pdf->Write(0, "CVR 1308 1387", '', 0, 'R');
+$pdf->Line(152.5, 12, 152.5, 74.5);
 
 //Invoice address
+$address = '';
+$address .= $faktura['navn'];
+if($faktura['att']) $address .= "\n"._('Attn.:').' '.$faktura['att'];
+if($faktura['adresse']) $address .= "\n".$faktura['adresse'];
+if($faktura['postbox']) $address .= "\n".$faktura['postbox'];
+if($faktura['postnr']) $address .= "\n".$faktura['postnr'].' '.$faktura['by'];
+else $address .= "\n".$faktura['by']; 
+if($faktura['land'] && $faktura['land'] != 'DK') {
+	require_once '../inc/countries.php';
+	$address .= "\n"._($countries[$faktura['land']]);
+}
+
 $pdf->SetMargins(19, 0, 0);
 $pdf->Write(0, "\n");
 $pdf->SetY(35);
 $pdf->SetFont('times', '', 11);
-$pdf->Write(0, "Sygehus Nord\nAtt: Indkøbskontoret\nKøgevej 7-13\n4000 Roskilde\nDanmark");
+$pdf->Write(0, trim($address));
 
 //Delivery address
-$pdf->SetMargins(110, 0, 0);
-$pdf->Write(0, "\n");
-$pdf->SetY(30.6);
-$pdf->SetFont('times', 'BI', 10);
-$pdf->Write(0, "Leveringsadresse:\n");
-$pdf->SetFont('times', '', 11);
-$pdf->Write(0, "Lene Andersen\nLeveringsvej 8\n4000 Roskilde\nDanmark");
+$address = '';
+$address .= $faktura['postname'];
+if($faktura['postatt']) $address .= "\n"._('Attn.:').' '.$faktura['postatt'];
+if($faktura['postaddress']) $address .= "\n".$faktura['postaddress'];
+if($faktura['postaddress2']) $address .= "\n".$faktura['postaddress2'];
+if($faktura['postpostbox']) $address .= "\n".$faktura['postpostbox'];
+if($faktura['postpostalcode']) $address .= "\n".$faktura['postpostalcode'].' '.$faktura['postcity'];
+elseif($faktura['postcity']) $address .= "\n".$faktura['postcity']; 
+if($faktura['land'] && $faktura['land'] != 'DK') {
+	require_once '../inc/countries.php';
+	$address .= "\n"._($countries[$faktura['land']]);
+}
+
+if($address) {
+	$pdf->SetMargins(110, 0, 0);
+	$pdf->Write(0, "\n");
+	$pdf->SetY(30.6);
+	$pdf->SetFont('times', 'BI', 10);
+	$pdf->Write(0, _('Delivery address:')."\n");
+	$pdf->SetFont('times', '', 11);
+	$pdf->Write(0, trim($address));
+}
 
 //Invoice info
 $pdf->SetFont('times', '', 10);
 $pdf->SetMargins(8, 9, 8);
 $pdf->Write(0, "\n");
 $pdf->SetY(90.5);
-$pdf->writeHTML("<strong>Dato:</strong> 26/05/2010       <strong>Vor ref.:</strong> CPG       <strong>Deres ref.:</strong> Hansen");
+$info = '<strong>'._('Date').':</strong> '.date(_('m/d/Y'), $faktura['date']);
+if($faktura['iref']) $info .= '       <strong>'._('Our ref.:').'</strong> '.$faktura['iref'];
+if($faktura['eref']) $info .= '       <strong>'._('Their ref.:').'</strong> '.$faktura['eref'];
+$pdf->writeHTML($info);
 
 //Invoice info
 $pdf->SetFont('times', '', 26);
 $pdf->Write(0, "\n");
 $pdf->SetY(85);
-$pdf->writeHTML("<strong>Online faktura</strong> 1547", false, false, false, false, 'R');
+$pdf->writeHTML("<strong>"._('Online Invoice')."</strong> ".$faktura['id'], false, false, false, false, 'R');
 
 //Invoice table
 $pdf->SetFont('times', '', 10);
@@ -95,78 +166,102 @@ $pdf->Cell(0, 10.5, '', 0, 1);
 //$pdf->SetY(93);
 
 //Header
-$pdf->Cell(24, 5, 'Antal', 1, 0, 'L');
-$pdf->Cell(106, 5, 'Benævnelse', 1, 0, 'L');
-$pdf->Cell(29, 5, 'á pris', 1, 0, 'R');
-$pdf->Cell(34, 5, 'Total', 1, 1, 'R');
+$pdf->Cell( 24, 5, _('Quantity'), 1, 0, 'L');
+$pdf->Cell(106, 5, _('Title'), 1, 0, 'L');
+$pdf->Cell( 29, 5, _('unit price'), 1, 0, 'R');
+$pdf->Cell( 34, 5, _('Total'), 1, 1, 'R');
 
 //Cells
-$pdf->Cell(24, 6, '1', 'RL', 0, 'R');
-$pdf->Cell(106, 6, 'Stetson hat', 'RL', 0, 'L');
-$pdf->Cell(29, 6, '319,20', 'RL', 0, 'R');
-$pdf->Cell(34, 6, '319,20', 'RL', 1, 'R');
+$netto = 0;
+$extralines = 0;
+foreach($faktura['values'] as $i => $value) {
+	
+	if($lines > 1) {
+		$lines -= 1;
+		$extralines += $lines;
+		$pdf->Cell( 24, 6*$lines, '', 'RL', 0);
+		$pdf->Cell(106, 6*$lines, '', 'RL', 0);
+		$pdf->Cell( 29, 6*$lines, '', 'RL', 0);
+		$pdf->Cell( 34, 6*$lines, '', 'RL', 1);
+	}
 
-$pdf->Cell(24, 6, '1', 'RL', 0, 'R');
-$pdf->Cell(106, 6, 'Stetson cap', 'RL', 0, 'L');
-$pdf->Cell(29, 6, '319,20', 'RL', 0, 'R');
-$pdf->Cell(34, 6, '319,20', 'RL', 1, 'R');
+	$netto += $value*$faktura['quantities'][$i];
 
-$pdf->Cell(24, 6, '1', 'RL', 0, 'R');
-$pdf->Cell(106, 6, 'Stetson cap', 'RL', 0, 'L');
-$pdf->Cell(29, 6, '159,20', 'RL', 0, 'R');
-$pdf->Cell(34, 6, '159,20', 'RL', 1, 'R');
-
-$pdf->Cell(24, 6, '1', 'RL', 0, 'R');
-$pdf->Cell(106, 6, 'Buff hoved og hals beklædning', 'RL', 0, 'L');
-$pdf->Cell(29, 6, '135,20', 'RL', 0, 'R');
-$pdf->Cell(34, 6, '135,20', 'RL', 1, 'R');
+	$pdf->Cell( 24, 6, $faktura['quantities'][$i], 'RL', 0, 'R');
+	$lines = $pdf->MultiCell(106, 6, htmlspecialchars_decode($faktura['products'][$i]), 'RL', '0', 0, 0, '', '', true, 0, false, true, 0);
+//	$pdf->Cell(106, 6, $faktura['products'][$i], 'RL', 0, 'L');
+	$pdf->Cell( 29, 6, number_format($value, 2, ',', ''), 'RL', 0, 'R');
+	$pdf->Cell( 34, 6, number_format($value*$faktura['quantities'][$i], 2, ',', ''), 'RL', 1, 'R');
+}
 
 //Spacing
-$pdf->Cell(24, 6*(16-4), '', 'RL', 0);
-$pdf->Cell(106, 6*(16-4), '', 'RL', 0);
-$pdf->Cell(29, 6*(16-4), '', 'RL', 0);
-$pdf->Cell(34, 6*(16-4), '', 'RL', 1);
+$pdf->Cell( 24, 6*(16-$i-$extralines), '', 'RL', 0);
+$pdf->Cell(106, 6*(16-$i-$extralines), '', 'RL', 0);
+$pdf->Cell( 29, 6*(16-$i-$extralines), '', 'RL', 0);
+$pdf->Cell( 34, 6*(16-$i-$extralines), '', 'RL', 1);
 
 //Footer
-$pdf->Cell(24, 6, '', 'RL', 0);
+/*
+$pdf->Cell( 24, 6, '', 'RL', 0);
 $pdf->Cell(106, 6, '', 'RL', 0);
-$pdf->Cell(29, 6, 'Nettobeløb', 'RL', 0, 'R');
-$pdf->Cell(34, 6, '932,80', 'RL', 1, 'R');
+$pdf->Cell( 29, 6, _('Net Amount'), 'RL', 0, 'R');
+$pdf->Cell( 34, 6, number_format($netto, 2, ',', ''), 'RL', 1, 'R');
+*/
+$pdf->Cell( 24, 6, '', 'RL', 0);
+$pdf->Cell(106, 6, ($faktura['momssats']*100).'%', 'RL', 0, 'R');
+//Her af moms
+$pdf->Cell( 29, 6, _('VAT Amount'), 'RL', 0, 'R');
+$pdf->Cell( 34, 6, number_format($netto*$faktura['momssats'], 2, ',', ''), 'RL', 1, 'R');
 
-$pdf->Cell(24, 6, '', 'RL', 0);
+$pdf->Cell( 24, 6, '', 'RL', 0);
 $pdf->Cell(106, 6, '', 'RL', 0);
-$pdf->Cell(29, 6, 'Forsendelse', 'RL', 0, 'R');
-$pdf->Cell(34, 6, '0,00', 'RL', 1, 'R');
-
-$pdf->Cell(24, 6, '', 'RL', 0);
-$pdf->Cell(106, 6, '25%', 'RL', 0, 'R');
-$pdf->Cell(29, 6, 'Momsbeløb', 'RL', 0, 'R');
-$pdf->Cell(34, 6, '233,20', 'RL', 1, 'R');
+//Forsendelse
+$pdf->Cell( 29, 6, _('Shipping'), 'RL', 0, 'R');
+$pdf->Cell( 34, 6, number_format($faktura['fragt'], 2, ',', ''), 'RL', 1, 'R');
 
 $pdf->SetFont('times', '', 10);
-$pdf->MultiCell(130, 8, "<strong>Betalingsbetingelser:</strong> Netto kontant ved faktura modtagelse.<small><br>Ved senere indbetaling end anførte frist vil der blive debiteret 2% rente pr. påbegyndt måned.</small>", 1, 'L', 0, 0, '', '', false, 1, true, false);
+$pdf->MultiCell(130, 8, '<strong>'._('Payment Terms:').'</strong> '._('Net cash at invoice reception.').'<small><br>'._('In case of payment later than the stated deadline, 2% interest will be added per. started months.').'</small>', 1, 'L', 0, 0, '', '', false, 1, true, false);
 $pdf->SetFont('times', 'B', 11);
-$pdf->Cell(29, 8, 'AT BETALE', 1, 0, 'C');
+$pdf->Cell(29, 8, _('TO PAY'), 1, 0, 'C');
 $pdf->SetFont('times', '', 11);
-$pdf->Cell(34, 8, '1166,00', 1, 1, 'R');
+$pdf->Cell(34, 8, number_format($faktura['amount'], 2, ',', ''), 1, 1, 'R');
 
 //Note
-$pdf->SetFont('times', 'B', 10);
-$pdf->Write(0, "\nNotat:\n");
-$pdf->SetFont('times', '', 10);
-$pdf->Write(0, "Ordrenr.: 192362\nRekvirent 081 - Onkologisk afdeling 081, Roskilde\nVaren er leveret til C. Lang");
+$note = '';
+if($faktura['status'] == 'accepted') {
+	$note .= _('Paid online');
+	if($faktura['paydate']) $note .= ' d. '.date(_('m/d/Y'), $faktura['paydate']);
+	$note .= "\n";
+} elseif($faktura['status'] == 'giro') {
+	$note .= _('Paid via giro');
+	if($faktura['paydate']) $note .= ' d. '.date(_('m/d/Y'), $faktura['paydate']);
+	$note .= "\n";
+} elseif($faktura['status'] == 'cash') {
+	$note .= _('Paid in cash');
+	if($faktura['paydate']) $note .= ' d. '.date(_('m/d/Y'), $faktura['paydate']);
+	$note .= "\n";
+}
 
-//Sign off
+$note .= $faktura['note'];
+
+if($note) {
+	$pdf->SetFont('times', 'B', 10);
+	$pdf->Write(0, "\n"._('Note:')."\n");
+	$pdf->SetFont('times', '', 10);
+	$pdf->Write(0, $note);
+}
+
+//Sign off'.$GLOBALS['_config']['address'].'
 $pdf->SetFont('times', 'B', 12);
 $pdf->SetMargins(137, 0, 0);
 $pdf->Write(0, "\n");
 $pdf->SetY(-52);
-$pdf->Write(0, "Med venlig hilsen\n\n\nCarsten P. Grølsted\nJagt og Fiskerimgasinet");
+$pdf->Write(0, _('Sincerely,')."\n\n\n".$faktura['clerk']."\n".$GLOBALS['_config']['site_name']);
 
 // ---------------------------------------------------------
 
 //Close and output PDF document
-$pdf->Output('Faktura-1547.pdf', 'I');
+$pdf->Output('Faktura-'.$faktura['id'].'.pdf', 'I');
 
 //============================================================+
 // END OF FILE                                                 
