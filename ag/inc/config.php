73173a2edb4f87cb41dad4cdae3dbb1bcf1e36ce
Anders Jenbo <anders@jenbo.dk>
2011-02-02 02:14:30 +0000

    git-svn-id: http://jof.unfuddle.com/svn/jof_cms@314 31fb0081-5fcf-4b11-800b-6304723eda88

--- ag/inc/config.php
+++ ag/inc/config.php
@@ -1,42 +1,20 @@
 <?php
-$GLOBALS['_config']['base_url'] = 'http://arms-gallery.dk';
-$GLOBALS['_config']['site_name'] = 'Arms Gallery';
-$GLOBALS['_config']['address'] = 'Nybrogade 26 - 30';
-$GLOBALS['_config']['postcode'] = '1203';
-$GLOBALS['_config']['city'] = 'København K';
-$GLOBALS['_config']['phone'] = '33 11 8338';
-$GLOBALS['_config']['fax'] = '33 13 8338';
+//FTP for makedir
+$GLOBALS['_config']['ftp_User'] = '';
+$GLOBALS['_config']['ftp_Pass'] = '';
+$GLOBALS['_config']['ftp_Root'] = '';
 
-$GLOBALS['_config']['email'][] = 'mail@arms-gallery.dk';
-$GLOBALS['_config']['emailpasswords'][] = '.45acp';
-$GLOBALS['_config']['emailsent'] = 'INBOX.Sent';
-$GLOBALS['_config']['imap'] = 'imap.arms-gallery.dk';
-$GLOBALS['_config']['imapport'] = '143';
+//Site color settings
+$GLOBALS['_config']['bgcolor'] = "FFFFFF";
+$GLOBALS['_config']['bgcolorR'] = 255;
+$GLOBALS['_config']['bgcolorG'] = 255;
+$GLOBALS['_config']['bgcolorB'] = 255;
 
-$GLOBALS['_config']['smtp'] = 'mailout.one.com';
-$GLOBALS['_config']['smtpport'] = 25;
-$GLOBALS['_config']['emailpassword'] = false;
+//Images
+$GLOBALS['_config']['thumb_width'] = 230;
+$GLOBALS['_config']['thumb_height'] = 96;
 
-$GLOBALS['_config']['interests'][] = 'Paintballbaner';
-$GLOBALS['_config']['interests'][] = 'Batterier';
-$GLOBALS['_config']['interests'][] = 'Buegrej';
-$GLOBALS['_config']['interests'][] = 'Taktisk materiel';
-$GLOBALS['_config']['interests'][] = 'Politiudstyr';
-$GLOBALS['_config']['interests'][] = 'Lygter';
-$GLOBALS['_config']['interests'][] = 'Knive og Tools';
-$GLOBALS['_config']['interests'][] = 'Luft- Haglvåben';
-$GLOBALS['_config']['interests'][] = 'Masker';
-$GLOBALS['_config']['interests'][] = 'Modelvåben';
-$GLOBALS['_config']['interests'][] = 'Paintball';
-$GLOBALS['_config']['interests'][] = 'Sigtemidler';
-$GLOBALS['_config']['interests'][] = 'Soft-Air-Guns';
-$GLOBALS['_config']['interests'][] = 'Sværd';
-$GLOBALS['_config']['interests'][] = 'Vagtudstyr';
-$GLOBALS['_config']['pbsid'] = '';
-$GLOBALS['_config']['pbspassword'] = 'salt24raej098';
-$GLOBALS['_config']['pbsfix'] = 'AG';
-$GLOBALS['_config']['mysql_server'] = 'arms-gallery.dk.mysql';
-$GLOBALS['_config']['mysql_user'] = 'arms_gallery_dk';
-$GLOBALS['_config']['mysql_password'] = 'hSKe3eDZ';
-$GLOBALS['_config']['mysql_database'] = 'arms_gallery_dk';
+$GLOBALS['_config']['text_width'] = 525;
+
+$GLOBALS['_config']['frontpage_width'] = 525;
 ?>
