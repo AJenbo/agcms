50712a3ffec2e5475d4b91bc47bd405c3c260570
Anders Jenbo <anders@jenbo.dk>
2016-11-08 09:40:32 +0100

    Implement barsic page entity

--- application/inc/Entity/Page.php
+++ application/inc/Entity/Page.php
@@ -1,27 +1,24 @@
 <?php
 
-class Category
+class Page
 {
     const TABLE_NAME = 'side';
 
     // Backed by DB
     private $id;
-    private $timestamp;
+    private $sku;
+    private $timeStamp;
     private $title;
     private $keywords;
-    private $price;
     private $html;
-    private $sku;
-    private $old_price;
-    private $requirement_id;
-    private $krav;
-    private $brand_id;
-    private $image_path;
-    private $price_type;
-    private $old_price_type;
-
-    // Runtime
-    private $visable;
+    private $excerpt;
+    private $imagePath;
+    private $requirementId;
+    private $brandId;
+    private $price;
+    private $oldPrice;
+    private $priceType;
+    private $oldPriceType;
 
     /**
      * Connect the database and set session to UTF-8 Danish
@@ -36,19 +33,19 @@ class Category
     {
         return [
             'id'             => $data['id'] ?: null,
+            'sku'            => $data['varenr'] ?: '',
             'timestamp'      => $data['dato'] ? strtotim($data['dato']) : 0,
             'title'          => $data['navn'] ?: '',
-            'keywords'       => $data['keywords'] ?? '',
-            'price'          => $data['pris'] ?? 0,
-            'html'           => $data['text'] ?? '',
-            'sku'            => $data['varenr'] ?? '',
-            'old_price'      => $data['for'] ?? 0,
-            'excerpt'        => $data['beskrivelse'] ?? '',
-            'requirement_id' => $data['krav'] ?? 0,
-            'brand_id'       => $data['maerke'] ?? 0,
-            'image_path'     => $data['billed'] ?? '',
-            'price_type'     => $data['fra'] ?? 0,
-            'old_price_type' => $data['burde'] ?? 0,
+            'keywords'       => $data['keywords'] ?: '',
+            'html'           => $data['text'] ?: '',
+            'excerpt'        => $data['beskrivelse'] ?: '',
+            'image_path'     => $data['billed'] ?: '',
+            'requirement_id' => $data['krav'] ?: 0,
+            'brand_id'       => $data['maerke'] ?: 0,
+            'price'          => $data['pris'] ?: 0,
+            'old_price'      => $data['for'] ?: 0,
+            'price_type'     => $data['fra'] ?: 0,
+            'old_price_type' => $data['burde'] ?: 0,
         ];
     }
 
@@ -69,6 +66,30 @@ class Category
         return $this->id;
     }
 
+    public function setSku(string $sku): self
+    {
+        $this->sku = $sku;
+
+        return $this;
+    }
+
+    public function getSku(): string
+    {
+        return $this->sku;
+    }
+
+    public function setTimeStamp(int $timeStamp): self
+    {
+        $this->timeStamp = $timeStamp;
+
+        return $this;
+    }
+
+    public function getTimeStamp(): int
+    {
+        return $this->timeStamp;
+    }
+
     public function setTitle(string $title): self
     {
         $this->title = $title;
@@ -81,6 +102,127 @@ class Category
         return $this->title;
     }
 
+    public function setKeywords(string $keywords): self
+    {
+        $this->keywords = $keywords;
+
+        return $this;
+    }
+
+    public function getKeywords(): string
+    {
+        return $this->keywords;
+    }
+
+    public function setHtml(string $html): self
+    {
+        $this->html = $html;
+
+        return $this;
+    }
+
+    public function getHtml(): string
+    {
+        return $this->html;
+    }
+
+    public function setExcerpt(string $excerpt): self
+    {
+        $this->excerpt = $excerpt;
+
+        return $this;
+    }
+
+    public function getExcerpt(): string
+    {
+        return $this->excerpt;
+    }
+
+    public function setImagePath(string $imagePath): self
+    {
+        $this->imagePath = $imagePath;
+
+        return $this;
+    }
+
+    public function getImagePath(): string
+    {
+        return $this->imagePath;
+    }
+
+    public function setRequirementId(int $requirementId): self
+    {
+        $this->requirementId = $requirementId;
+
+        return $this;
+    }
+
+    public function getRequirementId(): int
+    {
+        return $this->requirementId;
+    }
+
+    public function setBrandId(int $brandId): self
+    {
+        $this->brandId = $brandId;
+
+        return $this;
+    }
+
+    public function getBrandId(): int
+    {
+        return $this->brandId;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
+
+    public function setOldPrice(int $oldPrice): self
+    {
+        $this->oldPrice = $oldPrice;
+
+        return $this;
+    }
+
+    public function getOldPrice(): int
+    {
+        return $this->oldPrice;
+    }
+
+    public function setPriceType(int $priceType): self
+    {
+        $this->priceType = $priceType;
+
+        return $this;
+    }
+
+    public function getPriceType(): int
+    {
+        return $this->priceType;
+    }
+
+    public function setOldPriceType(int $oldPriceType): self
+    {
+        $this->oldPriceType = $oldPriceType;
+
+        return $this;
+    }
+
+    public function getOldPriceType(): int
+    {
+        return $this->oldPriceType;
+    }
+
+    // General methodes
     public function getSlug(bool $raw = false): string
     {
         $slug = 'side' . $kat->getId() . '-';
@@ -99,35 +241,54 @@ class Category
             db()->query(
                 "
                 INSERT INTO `" . self::TABLE_NAME . "` (
+                    `dato`,
                     `navn`,
-                    `bind`,
-                    `icon`,
-                    `vis`,
-                    `email`,
-                    `custom_sort_subs`,
-                    `order`
-                ) VALUES ('"
-                    . db()->esc($this->title) . "', "
-                    . $this->parentId . ", '"
-                    . db()->esc($this->iconPath) . "', "
-                    . $this->renderMode . ", '"
-                    . db()->esc($this->email) . "', "
-                    . $this->weightedChildren . ", "
-                    . $this->weight
-                . ")"
+                    `keywords`,
+                    `pris`,
+                    `text`,
+                    `varenr`,
+                    `for`,
+                    `beskrivelse`,
+                    `krav`,
+                    `maerke`,
+                    `billed`,
+                    `fra`,
+                    `burde`
+                ) VALUES (
+                    NOW(),
+                    '" . db()->esc($this->name) . "',
+                    '" . db()->esc($this->keywords) . "',
+                    " . $this->price . ",
+                    '" . db()->esc($this->html) . "',
+                    '" . db()->esc($this->sku) . "',
+                    " . $this->oldPrice . ",
+                    '" . db()->esc($this->excerpt) . "',
+                    " . $this->requirementId . ",
+                    " . $this->brandId . ",
+                    '" . db()->esc($this->imagePath) . "',
+                    " . $this->priceType . ",
+                    " . $this->oldPriceType . "
+                )
+                "
             );
             $this->setId(db()->insert_id);
         } else {
             db()->query(
                 "
                 UPDATE `" . self::TABLE_NAME ."`
-                SET `navn` = '" . db()->esc($this->title)
-                . "', `bind` = " . $this->parentId
-                . ", `icon` = '" . db()->esc($this->iconPath)
-                . "', `vis` = " . $this->renderMode
-                . ", `email` = '" . db()->esc($this->email)
-                . "', `custom_sort_subs` = " . $this->weightedChildren
-                . ", `order` = " . $this->weight
+                SET `dato` = NOW(),
+                `navn` = '" . db()->esc($this->title)
+                . "', `varenr` = '" . db()->esc($this->sku)
+                . "', `keywords` = '" . db()->esc($this->keywords)
+                . "', `text` = '" . db()->esc($this->html)
+                . "', `beskrivelse` = '" . db()->esc($this->excerpt)
+                . "', `billed` = '" . db()->esc($this->imagePath)
+                . "', `krav` = " . $this->requirementId
+                . ", `maerke` = " . $this->brandId
+                . ", `pris` = " . $this->price
+                . ", `for` = " . $this->oldPrice
+                . ", `fra` = " . $this->priceType
+                . ", `burde` = " . $this->old_priceType
                 . " WHERE `id` = " . $this->id
             );
         }
