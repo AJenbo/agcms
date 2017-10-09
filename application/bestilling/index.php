ac46f8eb8c5a92da7c3814bdefded8ee6134673d
Anders Jenbo <anders@jenbo.dk>
2017-10-09 12:09:13 +0200

    Fix syntax

--- application/bestilling/index.php
+++ application/bestilling/index.php
@@ -231,7 +231,7 @@ if (!empty($_SESSION['faktura']['quantities'])) {
 
         sendEmails(
             _('Online order #') . $invoice->getId(),
-            Render::output('email-order-notification', ['invoice' => $invoice]);
+            Render::render('email-order-notification', ['invoice' => $invoice]),
             $invoice->getEmail(),
             $invoice->getName()
         );
