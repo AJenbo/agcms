ea79b39a12cfabe14804f4c974613c06433044ce
Anders Jenbo <anders@jenbo.dk>
2017-10-14 03:10:04 +0200

    Combine js

--- application/theme/arms-gallery.dk/javascript/javascript.js
+++ application/theme/arms-gallery.dk/javascript/javascript.js
@@ -1,3 +1,11 @@
+function init()
+{
+    $('container').style.top = $('submenu').offsetTop + $('submenu').offsetHeight + 'px';
+    var activmenu = $('activmenu');
+    if(activmenu) {
+        $('menu').scrollTop = activmenu.offsetTop;
+    }
+}
 function $(id)
 {
     return document.getElementById(id);
--- application/theme/arms-gallery.dk/javascript/menufix.js
+++
@@ -1,8 +0,0 @@
-function init()
-{
-    $('container').style.top = $('submenu').offsetTop + $('submenu').offsetHeight + 'px';
-    var activmenu = $('activmenu');
-    if(activmenu) {
-        $('menu').scrollTop = activmenu.offsetTop;
-    }
-}
