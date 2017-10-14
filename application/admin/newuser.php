edc191061b758ca47acf6aab7328acce9f576c08
Anders Jenbo <anders@jenbo.dk>
2017-10-14 16:01:43 +0200

    Fix create new user

--- application/admin/newuser.php
+++ application/admin/newuser.php
@@ -5,11 +5,12 @@ use AGCMS\Render;
 require_once __DIR__ . '/../inc/Bootstrap.php';
 
 $message = '';
+$request = request();
 if ($request->isMethod('POST')) {
-    $fullname = request()->get('fullname');
-    $name = request()->get('name');
-    $password = request()->get('password');
-    $password2 = request()->get('password2');
+    $fullname = $request->get('fullname');
+    $name = $request->get('name');
+    $password = $request->get('password');
+    $password2 = $request->get('password2');
 
     if (!$fullname || !$name || !$password) {
         $message = _('All fields must be filled.');
