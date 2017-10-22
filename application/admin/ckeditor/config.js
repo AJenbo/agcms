7b8d82fdd2dc19850cbeccdea4b89958858f3611
Anders Jenbo <anders@jenbo.dk>
2017-10-22 03:53:08 +0200

    Configure CKEditor

--- application/admin/ckeditor/config.js
+++ application/admin/ckeditor/config.js
@@ -3,32 +3,50 @@
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here.
-	// For complete reference see:
-	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+CKEDITOR.editorConfig = function(config) {
+    // http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
-	// The toolbar groups arrangement, optimized for a single toolbar row.
-	config.toolbarGroups = [
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'forms' },
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'tools' },
-		{ name: 'others' },
-		{ name: 'about' }
-	];
+    config.allowedContent
+        = 'span{color}(liste,nypris,pris,xpris,gray); strong; em; u; sup; ul; ol; li; a[!href,target]; img[!src,alt,width,height](imgleft,imgright); p{text-align}; h2{text-align}; h3; cite; table; tr; td; video[!src,!controls,!width,!height]; audio[!src,!controls]';
+    // TODO convert audio and oembed plugin to use an <img> container instead of a simple div wrapper
+    config.extraAllowedContent
+        = 'div[data-*,!class](embeddedContent,oembed-provider-,oembed-provider-youtube,ckeditor-html5-audio);iframe[*]';
 
-	// The default plugins included in the basic setup define some buttons that
-	// are not needed in a basic editor. They are removed here.
-	config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,Underline,Strike,Subscript,Superscript';
+    config.toolbarGroups = [
+        { name : 'basicstyles', groups : [ 'basicstyles', 'colors', 'align', 'cleanup', 'others', 'links', 'list' ] },
+        { name : 'paragraph', groups : [ 'styles', 'insert', 'document', 'tools', 'mode' ] }
+    ];
 
-	// Dialog windows are also simplified.
-	config.removeDialogTabs = 'link:advanced';
+    config.removeButtons
+        = 'Format,Video,Html5audio,JustifyBlock,Link,searchCode,CommentSelectedRange,UncommentSelectedRange,autoFormat,AutoComplete';
+
+    config.tokenStart = '{{';
+    config.tokenEnd = '}}';
+    config.availableTokens = [];
+
+    config.uploadUrl = '/admin/upload/';
+
+    config.forcePasteAsPlainText = true;
+
+    config.baseHref = '/';
+
+    config.entities = false;
+    config.entities_additional = '';
+    config.entities_latin = false;
+    config.entities_processNumerical = false;
+
+    config.disableObjectResizing = true;
+    config.disableNativeSpellChecker = false;
+
+    config.language = 'da';
+    config.height = 420;
+
+    config.stylesSet = [
+        { name : 'Normal', element : 'p' }, { name : 'Overskrift 2', element : 'h2' },
+        { name : 'Overskrift 3', element : 'h3' },
+        { name : 'nypris', element : 'span', attributes : { 'class' : 'nypris' } },
+        { name : 'pris', element : 'span', attributes : { 'class' : 'pris' } },
+        { name : 'xpris', element : 'span', attributes : { 'class' : 'xpris' } },
+        { name : 'gray', element : 'span', attributes : { 'class' : 'gray' } }
+    ];
 };
