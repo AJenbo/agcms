b3d4e96e9055532f61067e25e99a3ea939800ae6
Anders Jenbo <anders@jenbo.dk>
2017-11-05 15:52:41 +0100

    Add phpdoc

--- application/inc/Interfaces/Renderable.php
+++ application/inc/Interfaces/Renderable.php
@@ -2,7 +2,17 @@
 
 interface Renderable
 {
+    /**
+     * Get page title
+     *
+     * @return string
+     */
     public function getTitle(): string;
 
+    /**
+     * Get page link
+     *
+     * @return string
+     */
     public function getCanonicalLink(): string;
 }
