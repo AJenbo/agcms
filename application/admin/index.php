449456a7b7391eeffa5863e4015f012d2d3263ce
Anders Jenbo <anders@jenbo.dk>
2017-11-22 22:44:40 +0100

    Add temp workaround for admin folder

--- application/admin/index.php
+++ application/admin/index.php
@@ -1,5 +1,9 @@
 <?php
 
+$_SERVER['SCRIPT_FILENAME'] = '/home/ajenbo/code/ArmsGallery/application/index.php';
+$_SERVER['SCRIPT_NAME'] = $_SERVER['SCRIPT_FILENAME'];
+include __DIR__ . '/../index.php';
+
 use AGCMS\Config;
 
 switch ('') {
