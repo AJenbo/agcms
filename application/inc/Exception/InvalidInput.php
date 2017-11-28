abf4064c2dbd3cd967fdae431e8410ca8a50e137
Anders Jenbo <anders@jenbo.dk>
2017-11-29 00:21:24 +0100

    Set propper default error code for InvalidInput

--- application/inc/Exception/InvalidInput.php
+++ application/inc/Exception/InvalidInput.php
@@ -1,7 +1,19 @@
 <?php namespace AGCMS\Exception;
 
 use Exception;
+use Throwable;
 
 class InvalidInput extends Exception
 {
+    /**
+     * Set up the exception.
+     *
+     * @param string    $message
+     * @param int       $code
+     * @param Throwable $previous
+     */
+    public function __construct(string $message, int $code = 422, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
