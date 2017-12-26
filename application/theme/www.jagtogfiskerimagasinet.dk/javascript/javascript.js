a1c8080f012387a47c4fb92f0642f942a2323c10
Anders Jenbo <anders@jenbo.dk>
2017-12-26 18:12:02 +0100

    Remove unused code

--- application/theme/www.jagtogfiskerimagasinet.dk/javascript/javascript.js
+++ application/theme/www.jagtogfiskerimagasinet.dk/javascript/javascript.js
@@ -2,11 +2,3 @@ function init() {
     shoppingCart.onupdate();
     openForigenLinksInNewWindow();
 }
-
-var krav;
-function openkrav(url) {
-    var left = (screen.width - 512) / 2;
-    var top = (screen.height - 395) / 2;
-    krav = window.open(url, "krav", "scrollbars=1,toolbar=0,width=512,height=395,left = " + left + ",top = " + top);
-    return false;
-}
