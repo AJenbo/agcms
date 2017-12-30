636b7ec7cfa70a5a7a30a95309f15b8d32dfcafa
Anders Jenbo <anders@jenbo.dk>
2017-12-30 01:47:20 +0100

    Remove ckeditor from repo (get it via build script)

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Install the wysiwyg editor
+wget --show-progress -q -O ckeditor.zip http://ckeditor.com/builder/download/4ef93c2297099222b5d2ba696dceaafa
+unzip ckeditor.zip
+rm ckeditor.zip
+sed -i 's/ckeditor\/plugins\/custimage\/dialogs\/dialogtest.html/\/admin\/explorer\/?return=ckeditor/g' ckeditor/plugins/custimage/dialogs/custimage.js
+rm -rf ckeditor/samples/
+rm -rf application/javascript/admin/ckeditor/
+mv ckeditor application/javascript/admin/
+
+#Install php dependencies
+cd application
+composer install -o --no-dev
