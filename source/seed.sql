0307d997bebf1004c27f1f6ee8b7141d8ef65b82
Anders Jenbo <anders@jenbo.dk>
2018-01-24 00:42:07 +0100

    Fix seed sql

--- source/seed.sql
+++ source/seed.sql
@@ -2,7 +2,7 @@ INSERT INTO `kat` (`id`, `navn`, `bind`, `vis`, `email`, `access`) VALUES
 (-1, 'Trash', NULL, 0, 'mail@example.com', ''),
 (1, 'Frontpage', NULL, 1, 'mail@example.com', '');
 UPDATE `kat` SET `id` = 0 WHERE `id` = 1;
-INSERT INTO `agcms`.`special` (`id`, `navn`, `dato`, `text`) VALUES
+INSERT INTO `special` (`id`, `navn`, `dato`, `text`) VALUES
 (1, 'Frontpage', NOW(), ''),
 (3, 'Terms & Conditions', NOW(), ''),
 (4, 'Cron', NOW(), '');
