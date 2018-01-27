7055418a83830eb30a966f021caa4df49c82e979
Anders Jenbo <anders@jenbo.dk>
2018-01-27 01:24:41 +0100

    Test aææ basic site routes

--- tests/Feature/Controller/SiteTest.php
+++ tests/Feature/Controller/SiteTest.php
@@ -10,4 +10,153 @@ class SiteTest extends TestCase
             ->assertResponseStatus(200)
             ->assertSee('<title>Frontpage</title>');
     }
+
+    public function testCategory(): void
+    {
+        $this->call('GET', '/kat1-Gallery-Category/')
+            ->assertResponseStatus(200)
+            ->assertSee('<title>Gallery Category</title>')
+            ->assertSee('<table cellspacing="0" id="liste">');
+    }
+
+    public function testCategoryBadSlug(): void
+    {
+        $this->call('GET', '/kat1-Wrong/')
+            ->assertResponseStatus(301)
+            ->assertRedirect('/kat1-Gallery-Category/');
+    }
+
+    public function testCategoryWrongId(): void
+    {
+        $this->call('GET', '/kat404-Gallery-Category/')
+            ->assertResponseStatus(303)
+            ->assertRedirect('/search/results/?q=Gallery%20Category&sogikke=&minpris=&maxpris=&maerke=0');
+    }
+
+    public function testCategoryList(): void
+    {
+        $this->call('GET', '/kat2-List-Category/')
+            ->assertResponseStatus(200)
+            ->assertSee('<title>List Category</title>')
+            ->assertSee('<table class="tabel">');
+    }
+
+    public function testCategoryEmpty(): void
+    {
+        $this->call('GET', '/kat3-Empty-Category/')
+            ->assertResponseStatus(303)
+            ->assertRedirect('/search/results/?q=Empty%20Category&sogikke=&minpris=&maxpris=&maerke=0');
+    }
+
+    public function testCategoryInactive(): void
+    {
+        $this->call('GET', '/kat4-Inactive-Category/')
+            ->assertResponseStatus(303)
+            ->assertRedirect('/search/results/?q=Inactive%20Category&sogikke=&minpris=&maxpris=&maerke=0');
+    }
+
+    public function testCategoryHidden(): void
+    {
+        $this->call('GET', '/kat5-Hidden-Category/')
+            ->assertResponseStatus(303)
+            ->assertRedirect('/search/results/?q=Hidden%20Category&sogikke=&minpris=&maxpris=&maerke=0');
+    }
+
+    public function testCategoryWithIndexPage(): void
+    {
+        $this->call('GET', '/kat6-Indexed-Category/')
+            ->assertResponseStatus(200)
+            ->assertSee('<title>Category Index Page</title>');
+    }
+
+    public function testPage(): void
+    {
+        $this->call('GET', '/kat1-Gallery-Category/side2-Page-1.html')
+            ->assertResponseStatus(200)
+            ->assertSee('<title>Page 1</title>');
+    }
+
+    public function testPageBadId(): void
+    {
+        $this->call('GET', '/kat1-Gallery-Category/side404-Page-1.html')
+            ->assertResponseStatus(301)
+            ->assertRedirect('/kat1-Gallery-Category/');
+    }
+
+    public function testPageBadSlug(): void
+    {
+        $this->call('GET', '/kat1-Gallery-Category/side2-Wrong.html')
+            ->assertResponseStatus(301)
+            ->assertRedirect('/kat1-Gallery-Category/side2-Page-1.html');
+    }
+
+    public function testRootPage(): void
+    {
+        $this->call('GET', '/side1-Root-Page.html')
+            ->assertResponseStatus(200)
+            ->assertSee('<title>Root Page</title>');
+    }
+
+    public function testRootPageBadSlug(): void
+    {
+        $this->call('GET', '/side1-Wrong.html')
+            ->assertResponseStatus(301)
+            ->assertRedirect('/side1-Root-Page.html');
+    }
+
+    public function testRootPageBadId(): void
+    {
+        $this->call('GET', '/side404-Root-Page.html')
+            ->assertResponseStatus(303)
+            ->assertRedirect('/search/results/?q=Root%20Page&sogikke=&minpris=&maxpris=&maerke=0');
+    }
+
+    public function testBrand(): void
+    {
+        $this->call('GET', '/mærke1-Test/')
+            ->assertResponseStatus(200)
+            ->assertSee('<title>Test</title>');
+    }
+
+    public function testBrandBadId(): void
+    {
+        $this->call('GET', '/mærke404-Test/')
+            ->assertResponseStatus(303)
+            ->assertRedirect('/search/results/?q=Test&sogikke=&minpris=&maxpris=&maerke=0');
+    }
+
+    public function testBrandBadSlug(): void
+    {
+        $this->call('GET', '/mærke1-Wrong/')
+            ->assertResponseStatus(301)
+            ->assertRedirect('/m%C3%A6rke1-Test/');
+    }
+
+    public function testBrandEmpty(): void
+    {
+        $this->call('GET', '/mærke2-Empty-Brand/')
+            ->assertResponseStatus(303)
+            ->assertRedirect('/search/results/?q=Empty%20Brand&sogikke=&minpris=&maxpris=&maerke=0');
+    }
+
+    public function testRequirement(): void
+    {
+        $this->call('GET', '/krav/1/Test.html')
+            ->assertResponseStatus(200)
+            ->assertSee('<title>Test</title>');
+    }
+
+    public function testRequirementBadId(): void
+    {
+        $this->call('GET', '/krav/404/Test.html')
+            ->assertResponseStatus(303)
+            ->assertRedirect('/search/results/?q=krav%20404%20Test&sogikke=&minpris=&maxpris=&maerke=0');
+    }
+
+    public function testRequirementBadSlug(): void
+    {
+        $this->call('GET', '/krav/1/Wrong.html')
+            ->assertResponseStatus(301)
+            ->assertRedirect('/krav/1/Test.html');
+    }
 }
--- tests/fixtures/seed.sql
+++ tests/fixtures/seed.sql
@@ -1,6 +1,12 @@
 INSERT INTO `kat` (`id`, `navn`, `bind`, `vis`, `email`, `access`) VALUES
 (-1, 'Trash', NULL, 0, 'mail@example.com', ''),
-(0, 'Frontpage', NULL, 1, 'mail@example.com', '');
+(0, 'Frontpage', NULL, 1, 'mail@example.com', ''),
+(1, 'Gallery Category', 0, 1, 'mail@example.com', ''),
+(2, 'List Category', 0, 2, 'mail@example.com', ''),
+(3, 'Empty Category', 0, 1, 'mail@example.com', ''),
+(4, 'Inactive Category', -1, 1, 'mail@example.com', ''),
+(5, 'Hidden Category', 0, 1, 'mail@example.com', ''),
+(6, 'Indexed Category', 0, 1, 'mail@example.com', '');
 INSERT INTO `special` (`id`, `navn`, `dato`, `text`) VALUES
 (1, 'Frontpage', 'now', ''),
 (3, 'Terms & Conditions', 'now', ''),
@@ -8,4 +14,21 @@ INSERT INTO `special` (`id`, `navn`, `dato`, `text`) VALUES
 -- password is 123456
 INSERT INTO `users` (`fullname`, `name`, `password`, `access`, `lastlogin`) VALUES
 ('test', 'test', '$2y$10$LmBhlJ6QHgLUKOSoqMSpp.V33uO9SXfTRigeTFA3I/ogXEvheR0gG', 1, 'now');
+INSERT INTO `sider` (`id`, `navn`, `text`, `beskrivelse`, `maerke`) VALUES
+('1', 'Root Page', '', '', null),
+('2', 'Page 1', '', '', null),
+('3', 'Product 1', '', '', 1),
+('4', 'Category Index Page', '', '', null);
+INSERT INTO `bind` (`side`, `kat`) VALUES
+('1', '0'),
+('2', '1'),
+('3', '1'),
+('2', '2'),
+('3', '2'),
+('4', '6');
 
+INSERT INTO `maerke` (`id`, `navn`) VALUES
+('1', 'Test'),
+('2', 'Empty Brand');
+INSERT INTO `krav` (`id`, `navn`, `text`) VALUES
+('1', 'Test', '');
