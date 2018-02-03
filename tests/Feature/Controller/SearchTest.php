2b54b9124911fc0e1102572f99a647dc0d1c0e10
Anders Jenbo <anders@jenbo.dk>
2018-02-03 01:53:13 +0100

    Test sqlite compatible parts of search

---
+++ tests/Feature/Controller/SearchTest.php
@@ -0,0 +1,28 @@
+<?php namespace AGCMS\Tests\Feature\Controller;
+
+use AGCMS\Tests\TestCase;
+
+class SearchTest extends TestCase
+{
+    public function testIndex(): void
+    {
+        $this->json('GET', '/search/')
+            ->assertResponseStatus(200)
+            ->assertSee('<form action="/search/results/" method="get">')
+            ->assertSee('<option value="1">Test</option>');
+    }
+
+    public function testResultsBrandRedirect(): void
+    {
+        $this->json('GET', '/search/results/?q=&varenr=&sogikke=&minpris=&maxpris=&maerke=1')
+            ->assertResponseStatus(301)
+            ->assertRedirect('/m%C3%A6rke1-Test/');
+    }
+
+    public function testResultsBrandRedirectInactive(): void
+    {
+        $this->json('GET', '/search/results/?q=&varenr=&sogikke=&minpris=&maxpris=&maerke=2')
+            ->assertResponseStatus(301)
+            ->assertRedirect('/search/');
+    }
+}
